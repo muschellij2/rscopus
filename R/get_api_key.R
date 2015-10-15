4fe0e53e71f5271fa91212b726cf46453a64e482
muschellij2 <muschellij2@gmail.com>
2015-10-15 07:07:33 -0400

    Get API Key data.

--- R/get_api_key.R
+++ R/get_api_key.R
@@ -3,13 +3,16 @@
 #' @description Determines if \code{option(elsevier_api_key)} is set.
 #' If not, it stops and returns an error.  If so, returns the value.
 #' @param api_key Elsvier API key
+#' @note You can either set the API key using
+#' \code{option(elsevier_api_key)} or have it accessible by
+#' \code{api_key = Sys.getenv('Elsevier_API')}.
 #' @return API key
 get_api_key = function(api_key = NULL) {
   if (is.null(api_key)) {
     api_key = getOption("elsevier_api_key")
   }
   if (is.null(api_key)) {
-    api_key = Sys.getenv("Elsevier_API", "")
+    api_key = Sys.getenv("Elsevier_API")
     if (api_key %in% ""){
       api_key = NULL
     }
