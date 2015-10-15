200a3a943f9771b663dc44e136c97ad4d75c6511
muschellij2 <muschellij2@gmail.com>
2015-10-15 07:32:36 -0400

    Adding affiliation search.

--- R/affil_search.R
+++ R/affil_search.R
@@ -1,25 +1,18 @@
 #' @title Search Author Content on SCOPUS
 #'
 #' @description Searches SCOPUS to get information about documents on an author.
-#' @param au_id Affiliation ID number
-#' @param api_key API Key for Elsevier
-#' @param http Address for scopus api
-#' @param count number of records to retrieve (below 100)
-#' @param verbose Print diagnostic messages
+#' @param affil_id Affiliation ID number
+#' @param searcher Identifer for Affiliation ID.  Do not change unless you
+#' know exactly what the API calls for.
 #' @param ... Arguments to be passed to \code{\link{GET}}
-#' @export
 #' @seealso \code{\link{get_author_info}}
 #' @return List of entries from SCOPUS
 affil_search <- function(
   affil_id, # Author ID number
-  api_key = NULL,
-  http = "http://api.elsevier.com/content/search/scopus",
-  count = 100, # number of records to retrieve (below 100)
-  verbose = TRUE,
+  searcher = "AF-ID",
   ...){
-
-  x = author_search(au_id = affil_id, searcher = "AF-ID", ...)
-
+  all_entries = author_search(au_id = affil_id,
+                              searcher = searcher, ...)
   return(all_entries)
 }
 
