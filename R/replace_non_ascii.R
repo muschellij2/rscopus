4825f657b80771e795a1ff549b6b891705e969db
muschellij2 <muschellij2@gmail.com>
2015-10-19 18:11:12 -0400

    Taking out non-ASCII.

--- R/replace_non_ascii.R
+++ R/replace_non_ascii.R
@@ -7,11 +7,6 @@
 #' @seealso Taken from \url{http://stackoverflow.com/questions/20495598/replace-accented-characters-in-r-with-non-accented-counterpart-utf-8-encoding}
 #' @return Chracter vector
 replace_non_ascii <- function(string){
-  #   unwanted_array = list('Š'='S', 'š'='s', 'Ž'='Z', 'ž'='z', 'À'='A', 'Á'='A', 'Â'='A', 'Ã'='A', 'Ä'='A', 'Å'='A', 'Æ'='A', 'Ç'='C', 'È'='E', 'É'='E',
-  #                         'Ê'='E', 'Ë'='E', 'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I', 'Ñ'='N', 'Ò'='O', 'Ó'='O', 'Ô'='O', 'Õ'='O', 'Ö'='O', 'Ø'='O', 'Ù'='U',
-  #                         'Ú'='U', 'Û'='U', 'Ü'='U', 'Ý'='Y', 'Þ'='B', 'ß'='Ss', 'à'='a', 'á'='a', 'â'='a', 'ã'='a', 'ä'='a', 'å'='a', 'æ'='a', 'ç'='c',
-  #                         'è'='e', 'é'='e', 'ê'='e', 'ë'='e', 'ì'='i', 'í'='i', 'î'='i', 'ï'='i', 'ð'='o', 'ñ'='n', 'ò'='o', 'ó'='o', 'ô'='o', 'õ'='o',
-  #                         'ö'='o', 'ø'='o', 'ù'='u', 'ú'='u', 'û'='u', 'ý'='y', 'ý'='y', 'þ'='b', 'ÿ'='y' )
   raws = structure(list(S = as.raw(c(0xc5, 0xa0)), s = as.raw(c(0xc5,
                                                                 0xa1)), Z = as.raw(c(0xc5, 0xbd)), z = as.raw(c(0xc5, 0xbe)),
                         A = as.raw(c(0xc3, 0x80)), A = as.raw(c(0xc3, 0x81)), A = as.raw(c(0xc3,
