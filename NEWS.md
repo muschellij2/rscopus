197ae8a0f686a1ef3dff0494aaeea460f8d01f70
muschellij2 <muschellij2@gmail.com>
2017-10-11 15:43:20 -0400

    Fixing tests -all green.

--- NEWS.md
+++ NEWS.md
@@ -2,5 +2,6 @@
 
 * Added a `NEWS.md` file to track changes to the package.
 
+* Author retrieval is now able to pass a person's name instead of an ID always.
 
 
--- tests/testthat/test_author_df.R
+++ tests/testthat/test_author_df.R
@@ -1,15 +1,23 @@
 context("Reading in Author Information")
 
+
+expect_pass = function(expr) {
+  # testthat::expect_that(object, !testthat::gives_warning())
+  testthat::expect_failure(expect_warning(expr))
+  testthat::expect_failure(expect_error(expr))
+}
+
+
 test_that("author_df_works", {
 
   if (have_api_key()) {
-    expect_success({
-      dd = author_df(au_id = "8858259000", all_author_info = TRUE)
+    expect_pass({
+        dd = author_df(au_id = "8858259000", all_author_info = TRUE)
     })
   }
 
   if (have_api_key()) {
-    expect_success({
+    expect_pass({
       dd = author_df(au_id = "8858259000", all_author_info = FALSE)
     })
   }
