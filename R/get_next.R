ac966416daf2edcbffd745f7b39d6e2cd148c8d2
muschellij2 <muschellij2@gmail.com>
2018-01-16 09:27:43 -0800

    Get_next url.

--- R/get_next.R
+++ R/get_next.R
@@ -2,6 +2,7 @@
 #'
 #' @param result Object (list) with an element named \code{content}, usually
 #' from \code{generic_elsevier_api}
+#' @param type The type of link requested
 #' @param ... Options passed to \code{\link{GET}}
 #'
 #'
@@ -29,24 +30,100 @@ get_links = function(result) {
 
 #' @rdname get_links
 #' @export
-next_url = function(result) {
+get_url = function(
+  result,
+  type = c("self", "first", "prev", "next", "last")) {
+  type = match.arg(type)
   links = get_links(result)
-  links = links[ links$ref %in% "next", ]
+  links = links[ links$ref %in% type, ]
   links = links$href
   links
 }
 
 #' @rdname get_links
 #' @export
+next_url = function(result) {
+  links = get_url(result, type = "next")
+  links
+}
+
+#' @rdname get_links
+#' @export
+last_url = function(result) {
+  links = get_url(result, type = "last")
+  links
+}
+
+#' @rdname get_links
+#' @export
+prev_url = function(result) {
+  links = get_url(result, type = "prev")
+  links
+}
+
+#' @rdname get_links
+#' @export
+self_url = function(result) {
+  links = get_url(result, type = "self")
+  links
+}
+
+#' @rdname get_links
+#' @export
+first_url = function(result) {
+  links = get_url(result, type = "first")
+  links
+}
+
+
+#' @rdname get_links
+#' @export
 #' @importFrom httr headers parse_url
-get_next = function(result, ...) {
+get_link_type = function(
+  result, ...,
+  type = c("self", "first", "prev", "next", "last")) {
   url = result$get_statement$url
   query = httr::parse_url(url)$query
-  http = next_url(result)
-  r = httr::GET(http,
-          query = query,
-          httr::headers(result$get_statement),
-          ...)
+  query$start = query$count = NULL
+  http = get_url(result, type = type)
+  r = httr::GET(
+    http,
+    query = query,
+    httr::headers(result$get_statement),
+    ...)
   cr = content(r)
   return(list(get_statement = r, content = cr))
 }
+
+
+#' @rdname get_links
+#' @export
+get_first = function(result, ...) {
+  get_link_type(result, ..., type = "first")
+}
+
+#' @rdname get_links
+#' @export
+get_last = function(result, ...) {
+  get_link_type(result, ..., type = "last")
+}
+
+#' @rdname get_links
+#' @export
+get_prev = function(result, ...) {
+  get_link_type(result, ..., type = "prev")
+}
+
+#' @rdname get_links
+#' @export
+get_next = function(result, ...) {
+  get_link_type(result, ..., type = "next")
+}
+
+
+#' @rdname get_links
+#' @export
+get_self = function(result, ...) {
+  get_link_type(result, ..., type = "self")
+}
+
