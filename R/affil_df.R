a8ea6529184dca05db40bd12fec418eb8518ed73
muschellij2 <muschellij2@gmail.com>
2018-01-31 23:51:18 -0500

    Fixing the output of affil_data.

--- R/affil_df.R
+++ R/affil_df.R
@@ -3,7 +3,10 @@
 #'
 #' @description Searches SCOPUS to get information about documents on an affiliation
 #' @param affil_id Affiliation ID number.
+#' @param affil_name affiliation name.  Disregarded if \code{affil_id} is
+#' specified
 #' @param verbose Print diagnostic messages
+#' @param api_key Elsevier API key
 #' @param ... Arguments to be passed to \code{\link{author_search}}
 #' @export
 #' @seealso \code{\link{get_author_info}}
@@ -11,12 +14,15 @@
 #' @note The \code{affil_data} command will return the list of all entries as well as
 #' the \code{data.frame}
 affil_df = function(
-  affil_id,
+  affil_id = NULL,
+  affil_name = NULL,
+  api_key = NULL,
   verbose = TRUE,
   ...){
 
   L = affil_data(
     affil_id = affil_id,
+    affil_name = affil_name,
     verbose = verbose,
     ... = ...)
   df = L$df
@@ -29,14 +35,23 @@ affil_df = function(
 #' @rdname affil_df
 #' @export
 affil_data = function(
-  affil_id,
+  affil_id = NULL,
+  affil_name = NULL,
+  api_key = NULL,
   verbose = TRUE,
   ...){
 
-#   entries = affil_search(
-#     affil_id = affil_id,
-#     verbose = verbose, ...)$entries
-
+  #   entries = affil_search(
+  #     affil_id = affil_id,
+  #     verbose = verbose, ...)$entries
+  if (is.null(affil_id)) {
+    res = process_affiliation_name(
+      affil_id = affil_id,
+      affil_name = affil_name,
+      api_key = api_key, verbose = verbose
+    )
+    affil_id = res$affil_id
+  }
   entries = author_search_by_affil(
     affil_id = affil_id,
     verbose = verbose, ...)$entries
@@ -45,7 +60,7 @@ affil_data = function(
   df = entries_to_df(entries = entries,
                      au_id = NULL,
                      verbose = verbose)
-  df$affid = affil_id
+  df$affil_id = affil_id
 
   L = list(entries = entries, df = df)
   return(L)
