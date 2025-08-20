6307b419d62ba53686a9531a8fad784752aad875
muschellij2 <muschellij2@gmail.com>
2025-08-20 10:21:29 -0400

    added views to abstract retrieval

--- R/abstract_retrieval.R
+++ R/abstract_retrieval.R
@@ -6,6 +6,10 @@
 #' @param identifier Type of identifier to use
 #' @param http_end any additional end to http statement.
 #' See \code{\link{generic_elsevier_api}}
+#' @param view the amount of information to view, see
+#' \url{https://dev.elsevier.com/sc_abstract_retrieval_views.html} for more info.
+#' There is a mismatch between these docs and the API, so `"BASIC"` and
+#' `"ENTITLED"` may have issues.
 #' @param ... Arguments to be passed to \code{\link{generic_elsevier_api}}
 #' @export
 #' @seealso \code{\link{generic_elsevier_api}}
@@ -23,6 +27,7 @@ abstract_retrieval <- function(
   id, # Identifier for abstract
   identifier = c("scopus_id", "eid", "doi", "pii", "pubmed_id", "pui", "group_id"), # type of identifier
   http_end = NULL, # any additional end to http statement.  See \code{\link{generic_elsevier_api}}
+  view = c("META", "META_ABS", "FULL", "REF", "ENTITLED", "BASIC"),
   ...
 ){
 
@@ -31,6 +36,7 @@ abstract_retrieval <- function(
   id = gsub("DOI:", "", id, fixed = TRUE)
   ender = paste0("/", paste(identifier, id, sep = "/"))
   ender = gsub("//", "/", ender)
+  view = match.arg(view)
 
   if (!is.null(http_end)) {
     ender = paste(ender, http_end, sep = "/")
